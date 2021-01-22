591874d945834e0b17d52872d80ce7492f2a6d38
GREENOVER <humains29@gmail.com>
2021-01-22 13:50:23 +0900

    feat: ForecastListTableViewCell 프로퍼티 추가

--- WeatherForecast/WeatherForecast/ForecatListTableViewCell.swift
+++ WeatherForecast/WeatherForecast/ForecatListTableViewCell.swift
@@ -1,16 +1,16 @@
 import UIKit
 
 class ForecatListTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
